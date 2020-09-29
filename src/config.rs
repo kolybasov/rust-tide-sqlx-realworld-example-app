b236c205a0f35e0ddcc372a0cdac7f4870144cb9
Mykola Basov <git@mbasov.com>
2020-09-29 09:06:51 +0300

    Use into isntead of to_string

--- src/config.rs
+++ src/config.rs
@@ -16,8 +16,8 @@ impl Config {
 
         let database_url = env::var("DATABASE_URL")?;
         let jwt_secret = env::var("JWT_SECRET")?;
-        let host = env::var("HOST").unwrap_or("127.0.0.1".to_string());
-        let port = env::var("PORT").unwrap_or("8080".to_string());
+        let host = env::var("HOST").unwrap_or("127.0.0.1".into());
+        let port = env::var("PORT").unwrap_or("8080".into());
 
         Ok(Config {
             database_url,
